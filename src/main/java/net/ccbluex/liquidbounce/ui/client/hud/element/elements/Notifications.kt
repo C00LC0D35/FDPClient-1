c076593ebcd42605f0806c0754bf642366bff1c6
C00LC0D35 <dg636gamez@gmail.com>
2022-08-16 12:02:42 -0400

    test

--- src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/Notifications.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/Notifications.kt
@@ -220,9 +220,9 @@ class Notification(
 
         // this for u david
         if(style.equals("LiquidBounce")) {
-            RenderUtils.drawRect(-textLength + -9F, 0F, textLength + 9F, -20F, Color(0, 0, 0, alpha))
-            RenderUtils.drawRect(-textLength + -9F, 0F, 5F, -20F, Color(0, 160, 255).rgb)
-            font.drawString("$title: $content", -textLength + -9F + 4F, -14F, Int.MAX_VALUE)
+            RenderUtils.drawRect(0f, 0F, 2 * textLength + 18F, -20F, Color(0, 0, 0, alpha))
+            RenderUtils.drawRect(0f, 0F, 5F, -20F, Color(0, 160, 255).rgb)
+            font.drawString("${title}: ${content}", -textLength + -9F + 4F, -14F, Int.MAX_VALUE)
             GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f)
         }
 
