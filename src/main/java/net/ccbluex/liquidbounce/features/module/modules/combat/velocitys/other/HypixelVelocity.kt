bbc0daeca016794943934f955cee0afcf516a0fc
Dg636 <dg636gamez@gmail.com>
2023-12-25 16:07:32 -0500

    Update HypixelVelocity.kt
    Signed-off-by: Dg636 <dg636gamez@gmail.com>

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/velocitys/other/HypixelVelocity.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/velocitys/other/HypixelVelocity.kt
@@ -1,19 +1,20 @@
 package net.ccbluex.liquidbounce.features.module.modules.combat.velocitys.other
 
 import net.ccbluex.liquidbounce.event.PacketEvent
+import net.ccbluex.liquidbounce.features.value.BoolValue
 import net.ccbluex.liquidbounce.features.module.modules.combat.velocitys.VelocityMode
 import net.minecraft.network.play.server.S12PacketEntityVelocity
 import net.ccbluex.liquidbounce.utils.misc.RandomUtils
 
 class HypixelVelocity : VelocityMode("Hypixel") {
 
+    private val bw = BoolValue("BedwarsMode", false)
+
     override fun onVelocityPacket(event: PacketEvent) {
         val packet = event.packet
         if(packet is S12PacketEntityVelocity) {
-          event.cancelEvent()
-          mc.thePlayer.motionY = packet.getMotionY().toDouble() / 8000.0
-          mc.thePlayer.motionX = 0.3 * packet.getMotionX().toDouble() / 8000.0
-          mc.thePlayer.motionZ = 0.3 * packet.getMotionY().toDouble() / 8000.0
+            event.cancelEvent()
+            if (!bw.get()) mc.thePlayer.motionY = packet.getMotionY().toDouble() / 8000.0
         }
     }
 }
