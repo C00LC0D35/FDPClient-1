b2c4b4c69941121d9bb7aa906546fc58c5e68ed1
Dg636 <dg636gamez@gmail.com>
2024-01-05 23:55:52 -0500

    Update MixinEntityPlayerSP.java
    Signed-off-by: Dg636 <dg636gamez@gmail.com>

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/entity/MixinEntityPlayerSP.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/entity/MixinEntityPlayerSP.java
@@ -14,7 +14,7 @@ import net.ccbluex.liquidbounce.features.module.modules.movement.Flight;
 import net.ccbluex.liquidbounce.features.module.modules.movement.InvMove;
 import net.ccbluex.liquidbounce.features.module.modules.movement.NoSlow;
 import net.ccbluex.liquidbounce.features.module.modules.movement.Sprint;
-import net.ccbluex.liquidbounce.features.module.modules.movement.Strafe;
+import net.ccbluex.liquidbounce.features.module.modules.movement.StrafeFix;
 import net.ccbluex.liquidbounce.features.module.modules.world.Scaffold;
 import net.ccbluex.liquidbounce.handler.protocol.ProtocolBase;
 import net.ccbluex.liquidbounce.ui.clickgui.ClickGui;
@@ -218,7 +218,7 @@ public abstract class MixinEntityPlayerSP extends MixinAbstractClientPlayer {
     @Inject(method = "onUpdateWalkingPlayer", at = @At("HEAD"), cancellable = true)
     public void onUpdateWalkingPlayer(CallbackInfo ci) {
         try {
-            final Strafe strafeFix = FDPClient.moduleManager.getModule(Strafe.class);
+            final StrafeFix strafeFix = FDPClient.moduleManager.getModule(StrafeFix.class);
             strafeFix.updateOverwrite();
             
             FDPClient.eventManager.callEvent(new MotionEvent(EventState.PRE));
